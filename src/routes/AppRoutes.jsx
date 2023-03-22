7165906f530c82821d3eceb191b4a1706fa9e30c
David Machuca <davidmachuca.dev@gmail.com>
2023-03-22 18:43:32 -0500

    Agregando etiqueta de componente page a la ruta /home

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -8,7 +8,7 @@ export const AppRoutes = () => {
     <BrowserRouter>
     <Routes>
       <Route path= "/" element= {<LandingPage/>} />
-      <Route path= "/home" element={HomePage}/>
+      <Route path= "/home" element={<HomePage />}/>
     </Routes>
     </BrowserRouter>
   )
